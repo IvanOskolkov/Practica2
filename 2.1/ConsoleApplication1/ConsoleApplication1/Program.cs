de7171d7f592294e5c75e4b82b4f9efe7fab7b28
IvanOskolkov <oskolkov427@gmail.com>
2024-04-26 13:38:04 +0700

    first commit

---
+++ 2.1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string J = Console.ReadLine(); 
+        string S = Console.ReadLine(); 
+
+        int count = 0; 
+
+        foreach (char jewel in J)
+        {
+            foreach (char stone in S)
+            {
+                if (jewel == stone)
+                {
+                    count++; 
+                }
+            }
+        }
+
+        Console.WriteLine(count); 
+    }
+}
---
+++ 2.2/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Train
+{
+    public int TrainNumber { get; set; }
+    
+    public string Destination { get; set; }
+    
+    public DateTime DepartureTime { get; set; }
+
+    public void TrainInfo()
+    {
+        Console.WriteLine($"Номер поезда:      {TrainNumber}");
+        Console.WriteLine($"Номер назначения:  {Destination}");
+        Console.WriteLine($"Время направления: {DepartureTime}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Train train = new Train
+        {
+            Destination = "Новосибириск",
+            TrainNumber = 254,
+            DepartureTime = new DateTime(2020, 10, 15, 12, 45, 0)
+        };
+
+        Console.WriteLine("Напишите номер поезда: ");
+        int userTrainNumber = int.Parse(Console.ReadLine());
+
+        if (userTrainNumber == train.TrainNumber)
+        {
+            train.TrainInfo();
+        }
+        else
+        {
+            Console.WriteLine("Поезда под таким номером нету");
+        }
+    }
+    
+}
