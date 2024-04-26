b1c5ab7fb856a4279618b235985ac5978c3481be
IvanOskolkov <oskolkov427@gmail.com>
2024-04-26 14:55:35 +0700

    last

---
+++ 2.5/ConsoleApplication2/ConsoleApplication2/DatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+
+namespace GarageConsoleApp;
+
+public static class DatabaseService
+{
+    private static NpgsqlConnection? _connection;
+    private static string GetConnectionString()
+    {
+        return @"Host=10.30.0.137;Port=5432;Database=gr622_osiev;Username=gr622_osiev;Password=Algoritmus1";
+    }
+    public static NpgsqlConnection GetSqlConnection()
+    {
+        if (_connection is null)
+        {
+            _connection = new NpgsqlConnection(GetConnectionString());
+            _connection.Open();
+        }
+        
+        return _connection;
+    }
+}
